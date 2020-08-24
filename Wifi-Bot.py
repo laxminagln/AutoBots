2d43de111fc673eadd9dfa7780c592e79af73599
nagln <laxminagln@gmail.com>
2020-08-24 13:43:24 +0530

    Update Wifi-Bot.py

--- Wifi-Bot.py
+++ Wifi-Bot.py
@@ -1,1 +1,8 @@
+import os
+import sys
 
+saved_networks = os.popen('netsh wlan show profiles').read()
+#print(saved_networks)
+
+available_networks = os.popen('netsh wlan show networks').read()
+#print(available_networks)
