59fc5b73fa5514cfb9ec53a33edd39e65f8a015c
nagln <laxminagln@gmail.com>
2020-08-24 06:10:52 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,1 +1,16 @@
-from selenium import webdriver
+from selenium import webdriver                                  #imports webdriver module from selenium
+
+browser = webdriver.Chrome("path to chromedriver")              #if Firefox, then path to firefox webdriver
+
+browser.get("address of website")                               #type the address of the website you want to login
+
+element_email = browser.find_element_by_id("id for username")   #find the id for username by inspecting and put it here
+element_email.send_keys("your username")                        #type your username here
+
+element_pw = browser.find_element_by_id("id for password")      #find the id for password by inspecting and put it here
+element_pw.send_keys("your password")                           #type your password here
+
+login = browser.find_element_by_id("id for login button")       #find the id for login by inspecting and put it here
+login.click()                                                   #clicks the login button
+
+#browser.quit()                                                 #quits the browser
